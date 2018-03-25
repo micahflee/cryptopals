aa7f81c79cc0dd9896657eeb9651c867c232db02
Micah Lee <micah@micahflee.com>
2018-03-24 21:33:24 -0700

    Run a whole challenge set at a time, instead of a challenge at a time

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,25 @@ fn main() {
     // Parse command line args
     let matches = App::new("Cryptopals")
         .about("Solutions to https://cryptopals.com/ challenges")
-        .arg(Arg::with_name("SET")
-            .help("Challenge set (int)")
+        .arg(Arg::with_name("CHALLENGE_SET")
+            .help("Challenge set to run (int)")
             .required(true)
             .index(1))
-        .arg(Arg::with_name("CHALLENGE")
-            .help("Challenge number within in the set (int)")
-            .required(true)
-            .index(2))
         .get_matches();
 
     // Validate set as integer
-    let set = match value_t!(matches, "SET", u32) {
+    let set = match value_t!(matches, "CHALLENGE_SET", u32) {
         Ok(v) => v,
         Err(_) => {
-            println!("SET must be an integer");
-            return;
-        }
-    };
-
-    // Validate challenge as integer
-    let challenge = match value_t!(matches, "CHALLENGE", u32) {
-        Ok(v) => v,
-        Err(_) => {
-            println!("CHALLENGE must be an integer");
+            println!("CHALLENGE_SET must be an integer");
             return;
         }
     };
 
     // Challenge set 1
     if set == 1 {
-        set1::index(challenge);
+        set1::index();
     } else {
-        println!("Sorry, I haven't implemented any challenges from that set");
+        println!("Sorry, I haven't implemented that challenge set");
     }
 }
--- src/set1.rs
+++ src/set1.rs
@@ -3,30 +3,13 @@ extern crate base64;
 
 use std::str;
 
-pub fn index(challenge: u32) {
-    println!("Challenge 1.{}", challenge);
-
-    if challenge == 1 {
-        challenge1();
-    }
-}
-
-fn hex_to_base64(hex_string: &str) -> Result<String, String> {
-    // Convert hex to Vec<u8>, an array of bytes
-    let bin = match hex::decode(hex_string) {
-        Ok(v) => v,
-        Err(_) => { return Err(String::from("Error converting hex to bytes")); }
-    };
-
-    println!("Decoded hex: {}", str::from_utf8(&bin).unwrap());
-
-    // Convert bin to base64
-    let base64_string = base64::encode(&bin);
-
-    Ok(base64_string)
+pub fn index() {
+    challenge1();
 }
 
 fn challenge1() {
+    println!("Challenge 1.1");
+
     let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let expected_base64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
@@ -38,7 +21,21 @@ fn challenge1() {
     assert_eq!(base64_string, expected_base64_string);
 }
 
+fn hex_to_base64(hex_string: &str) -> Result<String, String> {
+    // Convert hex to Vec<u8>, an array of bytes
+    let bin = match hex::decode(hex_string) {
+        Ok(v) => v,
+        Err(_) => { return Err(String::from("Error converting hex to bytes")); }
+    };
 
+    // Uncomment to print the hex
+    //println!("Decoded hex: {}", str::from_utf8(&bin).unwrap());
+
+    // Convert bin to base64
+    let base64_string = base64::encode(&bin);
+
+    Ok(base64_string)
+}
 
 #[cfg(test)]
 mod tests {
