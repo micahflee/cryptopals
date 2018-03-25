899e15ccef24185be9419699a491fc3934e6523e
Micah Lee <micah@micahflee.com>
2018-03-24 20:22:40 -0700

    Accept SET and CHALLENGE as command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+#[macro_use(value_t)]
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    // Parse command line args
+    let matches = App::new("Cryptopals")
+        .about("Solutions to https://cryptopals.com/ challenges")
+        .arg(Arg::with_name("SET")
+            .help("Challenge set (int)")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("CHALLENGE")
+            .help("Challenge number within in the set (int)")
+            .required(true)
+            .index(2))
+        .get_matches();
+
+    // Validate set as integer
+    let set = match value_t!(matches, "SET", u32) {
+        Ok(v) => v,
+        Err(_) => {
+            println!("SET must be an integer");
+            return;
+        }
+    };
+
+    // Validate challenge as integer
+    let challenge = match value_t!(matches, "CHALLENGE", u32) {
+        Ok(v) => v,
+        Err(_) => {
+            println!("CHALLENGE must be an integer");
+            return;
+        }
+    };
+
+    println!("Cryptopals, Challenge {}.{}", set, challenge);
 }
