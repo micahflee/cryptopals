97f24d86e82d2e2c138660406a69c0072f1ef6b8
Micah Lee <micah@micahflee.com>
2018-03-26 12:54:50 -0700

    Implemented challenge 1.5 (technically, I already implemented it in my xor_bytes function)

--- src/set1.rs
+++ src/set1.rs
@@ -22,6 +22,9 @@ pub fn index() {
 
     println!("\n{}", "Detect single-character XOR".blue().bold());
     challenge4();
+
+    println!("\n{}", "Implement repeating-key XOR".blue().bold());
+    challenge5();
 }
 
 fn challenge1() {
@@ -126,7 +129,19 @@ fn challenge4() {
             };
         }
     }
+}
 
+fn challenge5() {
+    // https://cryptopals.com/sets/1/challenges/5
+    let plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes().to_vec();
+    let key = "ICE".as_bytes().to_vec();
+    let ciphertext = xor_bytes(plaintext, key);
+    let ciphertext_hex = hex::encode(ciphertext);
+    assert_eq!(
+        ciphertext_hex,
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    );
+    println!("{}", ciphertext_hex);
 }
 
 fn hex_to_base64(hex_string: &str) -> Result<String, String> {
